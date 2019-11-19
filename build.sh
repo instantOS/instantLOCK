0580b310dd1e9b701730281a8d084033dc41d6ee
paperbenni <paperbenni@gmail.com>
2019-11-19 22:52:09 +0100

    fix theme applier

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ make clean &>/dev/null
 
 THEME=themes/${1:-dracula}.theme
 echo "$THEME"
-grep -q '.{,5}' <$THEME || { echo "theme not valid" && exit 1; }
+grep -Eq '.{5,}' <$THEME || { echo "theme not valid" && exit 1; }
 
 # input color
-sed -i 's/\[INPUT\].*,/'"[INPUT] = $(head -1 <$THEME),/" config.def.h
+sed -i 's/\[INPUT\].*,/[INPUT] = "#'"$(head -1 <$THEME)"'",/' config.def.h
 # wrong pw color
-sed -i 's/\[FAILED\].*,/'"[FAILED] = $(tail -1 <$THEME),/" config.def.h
+sed -i 's/\[FAILED\].*,/[FAILED] = "#'"$(tail -1 <$THEME)"'",/' config.def.h
 
 if [ -z "$2" ]; then
     make

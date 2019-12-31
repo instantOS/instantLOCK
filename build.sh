2a8da4c32065aef0a37c737bf5adf76ff69eb80a
paperbenni <paperbenni@gmail.com>
2019-12-31 14:21:45 +0100

    add manjaro theme and fix missing fonts

--- build.sh
+++ build.sh
@@ -11,6 +11,15 @@ sed -i 's/\[INPUT\].*,/[INPUT] = "#'"$(head -1 <$THEME)"'",/' config.def.h
 # wrong pw color
 sed -i 's/\[FAILED\].*,/[FAILED] = "#'"$(tail -1 <$THEME)"'",/' config.def.h
 
+# fix font
+if command -v slock &>/dev/null; then
+    if slock -f 2>&1 >/dev/null | grep -q '6x10'; then
+        sed -i 's/font_name = ".*"/font_name = "6x10"/' config.def.h
+    else
+        sed -i 's/font_name = ".*"/font_name = "6x13"/' config.def.h
+    fi
+fi
+
 if [ -z "$2" ]; then
     rm config.h &>/dev/null
     make
